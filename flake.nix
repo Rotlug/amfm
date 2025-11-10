c173b3778ee35be55af4ad3414a7a849d850e7c4
rotlug <rotlug77@gmail.com>
2025-11-10 21:57:18 +0200

    Add flake.nix for dependency tracking

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = with pkgs; [
+              rustc
+              cargo
+              rustlings
+              clippy
+
+              cargo-generate
+
+              pkg-config
+              openssl
+              gst_all_1.gstreamer
+              gst_all_1.gst-plugins-base
+              gst_all_1.gst-plugins-good
+              gst_all_1.gst-plugins-bad
+              gst_all_1.gst-plugins-ugly
+            ];
+          };
+        }
+    );
+}
