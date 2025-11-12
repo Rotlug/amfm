6d7dc7e239f0668383cb8d4a512d3942331bc065
rotlug <rotlug77@gmail.com>
2025-11-12 21:47:49 +0200

    Add new dirs to utils

--- antenna/src/utils.rs
+++ antenna/src/utils.rs
@@ -12,3 +12,32 @@ pub fn get_cache_dir() -> PathBuf {
 
     dir
 }
+
+pub fn get_temp_dir() -> PathBuf {
+    std::env::temp_dir()
+}
+
+pub fn get_music_directory() -> PathBuf {
+    dirs::audio_dir().expect("Audio directory should exist")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cache_dir() {
+        get_cache_dir();
+
+        assert!(fs::exists(dirs::cache_dir().unwrap().join("amfm")).unwrap());
+
+        // Check again to see that it doesn't crash when attempting
+        // To create the directory twice
+        get_cache_dir();
+    }
+
+    #[test]
+    fn music_dir() {
+        get_music_directory();
+    }
+}
